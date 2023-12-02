a45ec7f9124cc47f8222417eac45907d52b039d5
mark182182 <versalife11@hotmail.com>
2023-12-02 23:11:46 +0100

    Finalized the documentation

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
 from db.db import Database
-from raspi.lcd_i2c import LcdI2c
-from raspi.rfid_spi import RfidSpi
+#from raspi.lcd_i2c import LcdI2c
+#from raspi.rfid_spi import RfidSpi
 from repo.entry_repo import EntryRepository
 from repo.user_id_repo import UserIdRepository
 from repo.user_repo import UserRepository
@@ -13,14 +13,14 @@ app = Flask(__name__)
 db = Database()
 db.setup()
 
-lcdI2c = LcdI2c()
+#lcdI2c = LcdI2c()
 
 userRepo = UserRepository(db)
 rfidRepo = RfidRepository(db)
 userIdRepo = UserIdRepository(db, rfidRepo)
-entryRepo = EntryRepository(db, userIdRepo, lcdI2c)
+entryRepo = EntryRepository(db, userIdRepo)
 
 from route import user_route, rfid_route, user_id_route, entry_route
 
-rfidSpi = RfidSpi(entryRepo)
-lcdI2c.wait_for_input()
+#rfidSpi = RfidSpi(entryRepo)
+#lcdI2c.wait_for_input()
